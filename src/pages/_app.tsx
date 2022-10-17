304a12f49e89b7f08344838f07bf56ec8b3a7098
chaaerim <cofla411@ewhain.net>
2022-10-18 03:04:14 +0900

    :sparkles: feat: react-query initial setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import '../../styles/globals.css';
 import { RecoilRoot } from 'recoil';
+import type { AppProps } from 'next/app';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import React from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: unknown }>) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps?.dehydratedState}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
