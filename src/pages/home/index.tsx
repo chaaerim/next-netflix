656d49b0334fdd3d5e1eb18d93f9556cd72897c9
chaaerim <cofla411@ewhain.net>
2022-10-25 12:42:14 +0900

    :sparkles: feat: add homeButtons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import Footer from '@components/common/Footer';
 import Header from '@components/common/Header';
 import HomeBackground from '@components/homePage/HomeBackground';
+import HomeButtons from '@components/homePage/HomeButtons';
 
 function HomePage() {
   return (
     <>
       <Header />
-      <HomeBackground />
+      <HomeButtons />
       <Footer />
     </>
   );
