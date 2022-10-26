349915e1eaf01109badbdbb0740ed51bc2a8ee3e
chaaerim <cofla411@ewhain.net>
2022-10-27 06:01:44 +0900

    :bug: fix: import문 수정

--- src/components/detailPage/index.tsx
+++ src/components/detailPage/index.tsx
@@ -2,7 +2,7 @@ import { IComponentProps } from '@interfaces/interface';
 import styled from 'styled-components';
 import DetailDescription from './DetailDescription';
 import DetailImage from './DetailImage';
-import PlayButton from './playButton';
+import PlayButton from './PlayButton';
 
 function Detail({ children }: IComponentProps) {
   return (
